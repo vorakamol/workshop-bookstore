5492e027ba45bc3b5b52db88acf0e2e4bd0a56b7
vorakamol <49265385+vorakamol@users.noreply.github.com>
2021-08-28 15:02:33 +0700

    first

---
+++ src/main/java/demo/Book.java
@@ -0,0 +1,40 @@
+package demo;
+
+public class Book {
+    private int id;
+    private String name;
+    private int price;
+    private String store;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public void setStore(String store) {
+        this.store = store;
+    }
+}
---
+++ src/main/java/demo/BookStore.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class BookStore {
+    public static void main(String[] args) {
+        BookStore bookStore = new BookStore();
+        bookStore.process();
+    }
+
+    private void process() {
+
+    }
+}
